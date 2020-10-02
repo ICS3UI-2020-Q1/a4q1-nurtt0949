7a6226cfb18efcef81fe95a6d8b952936f1462b9
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-02 14:27:57 +0000

    Counts down to number

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Counts down to number
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner 
+    Scanner input = new Scanner(System.in);
+    //ask user for an integer to count down to
+    System.out.println("Please enter an integer to count down to from 100");
+    //declare a variable
+    int integer = input.nextInt();
+    //create variable to keep track of count down
+    int start = 100;
+    System.out.println("Count Down");
+    //start the count down
+    while(start > integer || start == integer){
+     System.out.println(start);
+     start = start - 5;
+
     
-  }
-}
+
+    } 
+    }
+    }
+  
